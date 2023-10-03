c8613e6a750c77327b4bc0ea255b41d40f738d78
YAMITRAO <work.aky@gmail.com>
2023-10-03 08:32:21 +0530

    filter aded to the project

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let formSelection = document.querySelector("#addForm");
 let taskSelection = document.querySelector("#item");
 let submitSelection = document.querySelector("#submit");
 let ul = document.querySelector("#items");
+let description = document.querySelector("#description")
 
 //add edit button
 
@@ -14,7 +15,7 @@ for(let i = 0; i<liNodeList.length; i++){
     let editButton = document.createElement("button");
 editButton.className="editButton";
 editButton.innerText = "Edit";
-    console.log(liNodeList[i]);
+    // console.log(liNodeList[i]);
      liNodeList[i].appendChild(editButton);
     // console.log(temp);
 }
@@ -25,13 +26,6 @@ formSelection.addEventListener('submit', onSubmit);
 ul.addEventListener('click', onClickBtn);
 
 
-   
-
-    
-
-    
-
-
 function onSubmit(e){
     e.preventDefault();
 
@@ -40,14 +34,21 @@ function onSubmit(e){
     let removebtn = document.createElement("button");
     removebtn.className = "btn btn-danger btn-sm float-right delete";
     removebtn.innerText ="X";
-    li.appendChild(removebtn)
+    
     li.className = "list-group-item";
-    let text = document.createTextNode(taskSelection.value);
+    // let text = document.createTextNode(taskSelection.value);
+    // let descriptionText = document.createTextNode(description.value);
+    let x = taskSelection.value +" "+ description.value;
+    
     let editButton = document.createElement("button");
 editButton.className="editButton";
-editButton.innerText = "Edit";
+editButton.textContent = "Edit";
+li.append(document.createTextNode(x));
+    // li.appendChild(text);
+    // li.appendChild(descriptionText);
+    li.appendChild(removebtn)
     li.appendChild(editButton);
-    li.appendChild(text);
+    
     ul.appendChild(li);
 
 }
@@ -57,12 +58,30 @@ function onClickBtn(e){
         console.log("clicked on button");
         e.target.parentElement.remove();
     };
-    
-
-
 
 }
 
-//
+//filter concept here 
+
+let filterSelection = document.querySelector("#filter");
+
+filterSelection.addEventListener('keyup', keyChange);
+
+function keyChange(e){
+
+    let enteredValue = e.target.value.toLowerCase();
+    let allLi = document.querySelectorAll(".list-group-item");
+    allLi.forEach( (item) => {
+        let y = item.firstChild.textContent.toLowerCase();
+        console.log(y.indexOf(enteredValue));
+        if(y.indexOf(enteredValue) != -1 ){
+            item.style.display = "block";
+        }
+        else{
+            item.style.display = "none";
+        }
+    })
+    
+}
 
 
